41ad92cbb17384328cd1a6c51a7ab7276eb91ce9
William Kirtlink <wmkirtlink@email.neit.edu>
2023-02-22 18:15:37 -0500

    Added search

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post("/saveGame", function(req,res){
 });
 
 app.get("/getGames", function(req,res){
-    Game.find({}).then(function(game){
+    Game.find({}).sort({"game":1}).then(function(game){
         //console.log({game});
         res.json({game});
     });
@@ -63,6 +63,19 @@ app.post("/updateGame", function(req, res){
         res.redirect('gameList.html');
     });
     
+})
+
+app.post("/searchGames", function(req, res){
+    console.log(req.body.game);
+    //res.redirect("searched.html?game=" + req.body.game);
+
+    Game.find({"game":req.body.game}).then(function(game){
+        //console.log(game[0].game);
+        //res.redirect("searched.html?game=" + game);
+        res.redirect("searched.html?id=" + game[0]._id + "&game=" + game[0].game);
+    }).catch(function(){
+        res.redirect("searched.html?game=");
+    })
 })
 
 app.post("/unity", function(req, res){
